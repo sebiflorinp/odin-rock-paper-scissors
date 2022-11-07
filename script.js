ccc7e5e997d13f62cdc8999d4e35667cb67d076f
sebiflorinp <87389406+sebiflorinp@users.noreply.github.com>
2022-11-07 20:29:49 +0200

    ADD getPlayerSelection and getComputerSelection functions

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-console.log("hello world!");
+function getComputerChoice()
+{
+    let randomNumber = Math.floor(Math.random()*100+1);
+    if(randomNumber>=66)
+        return "Rock";
+            else if(randomNumber>=33)
+                return "Paper";
+                    else
+                        return "Scissors";
+}
+
+function getPlayerSelection()
+{
+    console.log("Choose Rock Paper or Scissors");
+    let selection = prompt("Choose Rock Paper or Scissors");
+    while(selection != "Rock" && selection  != "Scissors" && selection != "Paper")
+    {
+        alert(`${selection} is not a valid choice, please try again`);
+        selection=prompt("Choose Rock Paper or Scissors");
+    }
+
+}
+
+getPlayerSelection();
