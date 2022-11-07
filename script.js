fa2fb60f7d5ee858cba803c9ea6c35cb9213f9e9
sebiflorinp <87389406+sebiflorinp@users.noreply.github.com>
2022-11-07 21:08:36 +0200

    ADD round function which plays a round a rock-paper-scissors

--- script.js
+++ script.js
@@ -1,24 +1,47 @@
 function getComputerChoice()
 {
-    let randomNumber = Math.floor(Math.random()*100+1);
+    let randomNumber = Math.floor(Math.random()*100)+1;
     if(randomNumber>=66)
-        return "Rock";
+        return "rock";
             else if(randomNumber>=33)
-                return "Paper";
+                return "paper";
                     else
-                        return "Scissors";
+                        return "scissors";
 }
 
 function getPlayerSelection()
 {
-    console.log("Choose Rock Paper or Scissors");
     let selection = prompt("Choose Rock Paper or Scissors");
     while(selection.toLowerCase() != "rock" && selection.toLowerCase()  != "scissors" && selection.toLowerCase() != "paper")
     {
         alert(`${selection} is not a valid choice, please try again`);
         selection=prompt("Choose Rock Paper or Scissors");
     }
-
+    return selection.toLowerCase();
 }
 
-getPlayerSelection();
+function round(playerSelection, computerSelection)
+{
+    let result;
+    switch(true){
+        case playerSelection == computerSelection:
+            result = 'Draw';
+            break;
+        case (playerSelection == 'paper' && computerSelection == 'scissors'):
+        case (playerSelection == 'scissors' && computerSelection == 'rock'):
+        case (playerSelection == 'rock' && computerSelection == 'paper'):
+            result = 'Computer';
+            break;
+        default:
+            result = 'Player';
+    }
+    if(result == 'Draw')
+        alert('It is a draw !!!');
+        else
+            {
+                alert(`${result} wins !!!`);
+            }
+}
+
+round(getPlayerSelection(),getComputerChoice());
+
