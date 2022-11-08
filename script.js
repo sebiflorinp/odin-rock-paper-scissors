adc9158671a79d2ff10802a2e9998a19d45c52f7
sebiflorinp <87389406+sebiflorinp@users.noreply.github.com>
2022-11-08 20:37:31 +0200

    Add game function and automatically play a game when the page has loaded

--- script.js
+++ script.js
@@ -20,28 +20,49 @@ function getPlayerSelection()
     return selection.toLowerCase();
 }
 
-function round(playerSelection, computerSelection)
+function playRound(playerSelection, computerSelection)
 {
     let result;
     switch(true){
         case playerSelection == computerSelection:
             result = 'Draw';
+            return result;
             break;
         case (playerSelection == 'paper' && computerSelection == 'scissors'):
         case (playerSelection == 'scissors' && computerSelection == 'rock'):
         case (playerSelection == 'rock' && computerSelection == 'paper'):
             result = 'Computer';
+            return result;
             break;
         default:
             result = 'Player';
+            return result;
     }
-    if(result == 'Draw')
-        alert('It is a draw !!!');
-        else
-            {
-                alert(`${result} wins !!!`);
-            }
 }
 
-round(getPlayerSelection(),getComputerChoice());
+function game()
+{
+    let playerScore = 0;
+    let computerScore = 0;
+    while(playerScore!=5 && computerScore!=5)
+    {
+        let result = playRound(getPlayerSelection(), getComputerChoice());
+        if(result=='Player')
+        {
+            playerScore++;
+        }
+        else if(result=='Computer')
+            {
+                computerScore++;
+            }
+        if(result=='Draw')
+            alert(`It's a draw!!!!
+Player score: ${playerScore} - Computer score: ${computerScore} `);
+        else
+        alert(`${result} wins!!!!
+Player score: ${playerScore} - Computer score: ${computerScore} `);
+    }
+    alert(playerScore>computerScore ? 'Player wins the game' : 'Cmputer wins the game');
+}
 
+game();
